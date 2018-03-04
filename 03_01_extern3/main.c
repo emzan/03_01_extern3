9775810b63d94fe217d9072d6a0af3182e08e8e2
emzan <zanettiemilio@hotmail.com>
2018-03-04 13:02:39 +0800

    Initial Commit

---
+++ 03_01_extern3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  03_01_extern3
+//
+//  Created by George Martinson on 04/03/2018.
+//  Copyright © 2018 kuangchi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
