b3f5b9cfb2722e3a1c1c40f1f520309335f9593b
emzan <zanettiemilio@hotmail.com>
2018-03-04 16:33:34 +0800

    withError

--- 03_01_extern3/03_01_extern3_manipulate.c
+++ 03_01_extern3/03_01_extern3_manipulate.c
@@ -10,17 +10,19 @@ void displayData(void)
     int x;
     
     printf("Data Set:\n");
-    for (x=0; x<5; x++) {
+    for (x=0; x<5; x++)
         printf("%2d\t",data[x]);
         putchar('\n');
     }
     
     void manipulateData(void)
     {
+        int x;
+        
         printf("Data Manipulated:\n");
-        for (x=0; x<5; x++) {
+        for (x=0; x<5; x++)
             printf("%2d\t",data[x]);
             putchar('\n');
-    }
+    
 }
-}
+
