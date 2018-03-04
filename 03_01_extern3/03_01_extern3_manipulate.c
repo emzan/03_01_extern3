9414d3e3109b4edbf7692883567702f50de811ad
emzan <zanettiemilio@hotmail.com>
2018-03-04 16:25:39 +0800

    toBeReviewed

--- 03_01_extern3/03_01_extern3_manipulate.c
+++ 03_01_extern3/03_01_extern3_manipulate.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+void displayData(void);
+void manipulateData(void);
+
+extern int data[];
+
+void displayData(void)
+{
+    int x;
+    
+    printf("Data Set:\n");
+    for (x=0; x<5; x++) {
+        printf("%2d\t",data[x]);
+        putchar('\n');
+    }
+    
+    void manipulateData(void)
+    {
+        printf("Data Manipulated:\n");
+        for (x=0; x<5; x++) {
+            printf("%2d\t",data[x]);
+            putchar('\n');
+    }
+}
+}
